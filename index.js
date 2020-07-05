c90ee0f1156b5b61e96efbd3f6ed8493457cec8a
anguianoo <njanguiano@gmail.com>
2020-07-05 11:49:09 -0700

    updated

--- index.js
+++ index.js
@@ -82,8 +82,16 @@ function showProjects(){
     })
 }
 */
-function make(){
-    click();
+var myCustomScrollbar = document.querySelector('.my-custom-scrollbar');
+var ps = new PerfectScrollbar(myCustomScrollbar);
 
+var scrollbarY = myCustomScrollbar.querySelector('.ps__rail-y');
+
+myCustomScrollbar.onscroll = function () {
+  scrollbarY.style.cssText = `top: ${this.scrollTop}px!important; height: 400px; right: ${-this.scrollLeft}px`;
 }
-make();
+// function make(){
+//     click();
+
+// }
+// make();
